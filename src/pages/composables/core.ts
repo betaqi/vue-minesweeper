1609713915c8256eb28766e7782dc2a5777ed381
贝塔棋 <3188864257@qq.com>
2023-01-15 19:40:16 +0800

    refactor: to class

---
+++ src/pages/composables/core.ts
@@ -0,0 +1,136 @@
+import type { Ref } from 'vue'
+import type { BlockState } from './types'
+
+const directions = [
+  [1, 1],
+  [1, 0],
+  [0, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, 0],
+  [0, -1],
+  [-1, -1],
+]
+
+export class Game {
+  state = ref() as Ref<BlockState[][]>
+  constructor(
+    public width: number,
+    public height: number,
+  ) {
+    this.reset()
+  }
+
+  reset(
+    width = this.width,
+    height = this.height,
+  ) {
+    this.width = width
+    this.height = height
+    this.state.value = Array.from({ length: this.height }, (_, y) =>
+      Array.from({ length: this.width }, (_, x): BlockState => ({
+        x,
+        y,
+        adjacentMines: 0,
+        revealed: false,
+      }),
+      ),
+    )
+    nextTick(() => {
+      this.makerMines()
+      this.updateNums()
+    })
+  }
+
+  updateNums() {
+    this.state.value.forEach((row: BlockState[]) => {
+      row.forEach((block: BlockState) => {
+        if (block.mine)
+          return
+        for (const [dx, dy] of directions) {
+          const x2 = block.x + dx
+          const y2 = block.y + dy
+          if (x2 < 0 || x2 >= this.width || y2 < 0 || y2 >= this.height)
+            continue
+          if (this.state.value[y2][x2].mine)
+            block.adjacentMines += 1
+        }
+      })
+    })
+  }
+
+  makerMines() {
+    for (const row of this.state.value) {
+      for (const block of row)
+        block.mine = Math.random() < 0.1
+    }
+  }
+
+  onClick(block: BlockState) {
+    if (block.flagged)
+      return
+    block.revealed = true
+    if (block.mine) {
+      this.revealedMine()
+      return
+    }
+    // this.checkGameState()
+    this.revealedSibling(block)
+  }
+
+  onRightClick(block: BlockState) {
+    if (block.revealed)
+      return
+    block.flagged = !block.flagged
+    // this.checkGameState()
+  }
+
+  revealedMine() {
+    this.state.value.forEach((row) => {
+      row.forEach((b) => {
+        if (b.mine)
+          b.revealed = true
+      })
+    })
+    nextTick(() => {
+      alert('GAME OVER')
+    })
+  }
+
+  revealedSibling(block: BlockState) {
+    if (block.adjacentMines)
+      return
+
+    this.getSibling(block)
+      // .filter(item => !item.adjacentMines)
+      .forEach((b) => {
+        if (!b.revealed) {
+          b.revealed = true
+          this.revealedSibling(b)
+        }
+      })
+  }
+
+  getSibling(block: BlockState) {
+    return directions.map(([dx, dy]) => {
+      const x = block.x - dx
+      const y = block.y - dy
+      if (x < 0 || x >= this.width || y < 0 || y >= this.height)
+        return undefined
+      return this.state.value[y][x]
+    }).filter(Boolean) as BlockState[]
+  }
+
+  checkGameState() {
+    const blocks = this.state.value.flat()
+
+    if (blocks.filter(block => block.mine).every(b => b.flagged))
+      alert('You Win!')
+
+    if (blocks.every(block => block.revealed || block.flagged || block.mine)) {
+      if (blocks.some(block => !block.revealed && block.mine))
+        alert('You Win!')
+    }
+  }
+}
+
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,132 +1,12 @@
 <script setup lang="ts">
+import { Game } from './composables/core'
+import type { BlockState, Difficulty } from './composables/types'
 defineOptions({
   name: 'IndexPage',
 })
-interface DifficultyMap {
-  [key: string]: number
-}
-
-interface BlockState {
-  x: number
-  y: number
-  mine?: boolean // 炸弹
-  flagged?: boolean // 标记
-  revealed: boolean // 翻开
-  adjacentMines: number // 相邻的地雷
-}
-
-const difficulty = ['low', 'mid', 'high']
-const state = ref(initState())
-let difficultyLevel = $ref('low')
-const difficultyMap: DifficultyMap = {
-  low: 10,
-  mid: 15,
-  high: 20,
-}
-
-function initState(level = 10) {
-  const data = Array.from({ length: level }, (_, y) =>
-    Array.from({ length: level }, (_, x): BlockState => ({
-      x,
-      y,
-      adjacentMines: 0,
-      revealed: false,
-    }),
-    ),
-  )
-  nextTick(() => {
-    makerMines()
-    updateNums()
-  })
-  return data
-}
-
-const directions = [
-  [1, 1],
-  [1, 0],
-  [0, 1],
-  [1, -1],
-  [-1, 1],
-  [-1, 0],
-  [0, -1],
-  [-1, -1],
-]
-function updateNums() {
-  state.value.forEach((row: BlockState[]) => {
-    row.forEach((block: BlockState) => {
-      if (block.mine)
-        return
-      for (const [dx, dy] of directions) {
-        const x2 = block.x + dx
-        const y2 = block.y + dy
-        if (x2 < 0 || x2 >= difficultyMap[difficultyLevel] || y2 < 0 || y2 >= difficultyMap[difficultyLevel])
-          continue
-        if (state.value[y2][x2].mine)
-          block.adjacentMines += 1
-      }
-    })
-  })
-}
-
-function makerMines() {
-  for (const row of state.value) {
-    for (const block of row)
-      block.mine = Math.random() < 0.2
-  }
-}
-
-function onClick(block: BlockState) {
-  block.revealed = true
-  if (block.mine) {
-    revealedMine()
-    return
-  }
-  checkGameState()
-  revealedSibling(block)
-}
-
-function onRightClick(block: BlockState) {
-  if (block.revealed)
-    return
-  block.flagged = !block.flagged
-  checkGameState()
-}
-
-function revealedMine() {
-  state.value.forEach((row) => {
-    row.forEach((b) => {
-      if (b.mine)
-        b.revealed = true
-    })
-  })
-  nextTick(() => {
-    alert('GAME OVER')
-  })
-}
-
-function revealedSibling(block: BlockState) {
-  if (block.adjacentMines)
-    return
-
-  getSibling(block)
-    // .filter(item => !item.adjacentMines)
-    .forEach((b) => {
-      if (!b.revealed) {
-        b.revealed = true
-        revealedSibling(b)
-      }
-    })
-}
-
-function getSibling(block: BlockState) {
-  return directions.map(([dx, dy]) => {
-    const x = block.x - dx
-    const y = block.y - dy
-    if (x < 0 || x >= difficultyMap[difficultyLevel] || y < 0 || y >= difficultyMap[difficultyLevel])
-      return undefined
-    return state.value[y][x]
-  }).filter(Boolean) as BlockState[]
-}
+const difficulty: Difficulty[] = ['easy', 'medium', 'hard']
+const game = new Game(12, 12)
+const state = game.state
 
 const blockColors = [
   'text-transparent border-transparent',
@@ -144,32 +24,37 @@ function getBlockClass(block: BlockState) {
   return block.mine ? 'text-red' : blockColors[block.adjacentMines]
 }
 
-function changeDifficulty(level: string) {
-  difficultyLevel = level
-  state.value = initState(difficultyMap[difficultyLevel])
-}
-
-function checkGameState() {
-  const blocks = state.value.flat()
-
-  if (blocks.filter(block => block.mine).every(b => b.flagged))
-    alert('You Win!')
-
-  if (blocks.every(block => block.revealed || block.flagged || block.mine)) {
-    if (blocks.some(block => !block.revealed && block.mine))
-      alert('You Win!')
+function newGame(difficulty: 'easy' | 'medium' | 'hard') {
+  switch (difficulty) {
+    case 'easy':
+      game.reset(9, 9)
+      break
+    case 'medium':
+      game.reset(16, 16)
+      break
+    case 'hard':
+      game.reset(16, 30)
+      break
   }
 }
+watchEffect(() => {
+  game.checkGameState()
+})
 </script>
 
 <template>
-  <button
-    v-for="item of difficulty" :key="item"
-    btn m-3 text-sm mt-8
-    @click="changeDifficulty(item)"
-  >
-    {{ item }}
-  </button>
+  <div flex="~ gap1" justify-center p4>
+    <button btn @click="game.reset()">
+      New Game
+    </button>
+    <button
+      v-for="(item, index) of difficulty" :key="index"
+      btn
+      @click="newGame(item)"
+    >
+      {{ item }}
+    </button>
+  </div>
   <div
     v-for="(row, y) of state" :key="y"
     flex="~" justify-center
@@ -178,9 +63,9 @@ function checkGameState() {
       v-for="block of row" :key="block.x"
       w-10 h-10
       border="1 gray-400/10"
-      :class="getBlockClass(block)"
-      @click="onClick(block)"
-      @contextmenu.prevent="onRightClick(block)"
+      :class=" getBlockClass(block)"
+      @click=" game.onClick(block)"
+      @contextmenu.prevent=" game.onRightClick(block)"
     >
       <template v-if="block.flagged">
         🚩
